f90de4f55e0903649525fd0f296469cf6afce273
Skyler Saleh <skylersaleh@gmail.com>
2022-05-20 21:37:28 -0700

    【Frontend】New menu bar layout and theme

--- src/main.c
+++ src/main.c
@@ -1087,6 +1087,89 @@ void se_update_frame() {
   sb_poll_controller_input(&emu_state.joy);
   se_draw_emulated_system_screen();
 }
+void se_imgui_theme()
+{
+  ImVec4* colors = igGetStyle()->Colors;
+  colors[ImGuiCol_Text]                   = (ImVec4){1.00f, 1.00f, 1.00f, 1.00f};
+  colors[ImGuiCol_TextDisabled]           = (ImVec4){0.50f, 0.50f, 0.50f, 1.00f};
+  colors[ImGuiCol_WindowBg]               = (ImVec4){0.14f, 0.14f, 0.14f, 1.00f};
+  colors[ImGuiCol_ChildBg]                = (ImVec4){0.00f, 0.00f, 0.00f, 0.00f};
+  colors[ImGuiCol_PopupBg]                = (ImVec4){0.19f, 0.19f, 0.19f, 0.92f};
+  colors[ImGuiCol_Border]                 = (ImVec4){0.3f, 0.3f, 0.3f, 1.0f};
+  colors[ImGuiCol_BorderShadow]           = (ImVec4){0.00f, 0.00f, 0.00f, 0.24f};
+  colors[ImGuiCol_FrameBg]                = (ImVec4){0.05f, 0.05f, 0.05f, 0.54f};
+  colors[ImGuiCol_FrameBgHovered]         = (ImVec4){0.19f, 0.19f, 0.19f, 1.0f};
+  colors[ImGuiCol_FrameBgActive]          = (ImVec4){0.20f, 0.22f, 0.23f, 1.00f};
+  colors[ImGuiCol_TitleBg]                = (ImVec4){0.00f, 0.00f, 0.00f, 1.00f};
+  colors[ImGuiCol_TitleBgActive]          = (ImVec4){0.06f, 0.06f, 0.06f, 1.00f};
+  colors[ImGuiCol_TitleBgCollapsed]       = (ImVec4){0.00f, 0.00f, 0.00f, 1.00f};
+  colors[ImGuiCol_MenuBarBg]              = (ImVec4){0.10f, 0.10f, 0.10f, 1.00f};
+  colors[ImGuiCol_ScrollbarBg]            = (ImVec4){0.05f, 0.05f, 0.05f, 0.54f};
+  colors[ImGuiCol_ScrollbarGrab]          = (ImVec4){0.34f, 0.34f, 0.34f, 0.54f};
+  colors[ImGuiCol_ScrollbarGrabHovered]   = (ImVec4){0.40f, 0.40f, 0.40f, 0.54f};
+  colors[ImGuiCol_ScrollbarGrabActive]    = (ImVec4){0.56f, 0.56f, 0.56f, 0.54f};
+  colors[ImGuiCol_CheckMark]              = (ImVec4){0.33f, 0.67f, 0.86f, 1.00f};
+  colors[ImGuiCol_SliderGrab]             = (ImVec4){0.34f, 0.34f, 0.34f, 0.54f};
+  colors[ImGuiCol_SliderGrabActive]       = (ImVec4){0.56f, 0.56f, 0.56f, 0.54f};
+  colors[ImGuiCol_Button]                 = (ImVec4){0.25f, 0.25f, 0.25f, 1.00f};
+  colors[ImGuiCol_ButtonHovered]          = (ImVec4){0.19f, 0.19f, 0.19f, 0.54f};
+  colors[ImGuiCol_ButtonActive]           = (ImVec4){0.4f, 0.4f, 0.4f, 1.00f};
+  colors[ImGuiCol_Header]                 = (ImVec4){0.00f, 0.00f, 0.00f, 0.52f};
+  colors[ImGuiCol_HeaderHovered]          = (ImVec4){0.00f, 0.00f, 0.00f, 0.36f};
+  colors[ImGuiCol_HeaderActive]           = (ImVec4){0.20f, 0.22f, 0.23f, 0.33f};
+  colors[ImGuiCol_Separator]              = (ImVec4){0.28f, 0.28f, 0.28f, 0.29f};
+  colors[ImGuiCol_SeparatorHovered]       = (ImVec4){0.44f, 0.44f, 0.44f, 0.29f};
+  colors[ImGuiCol_SeparatorActive]        = (ImVec4){0.40f, 0.44f, 0.47f, 1.00f};
+  colors[ImGuiCol_ResizeGrip]             = (ImVec4){0.28f, 0.28f, 0.28f, 0.29f};
+  colors[ImGuiCol_ResizeGripHovered]      = (ImVec4){0.44f, 0.44f, 0.44f, 0.29f};
+  colors[ImGuiCol_ResizeGripActive]       = (ImVec4){0.40f, 0.44f, 0.47f, 1.00f};
+  colors[ImGuiCol_Tab]                    = (ImVec4){0.00f, 0.00f, 0.00f, 0.52f};
+  colors[ImGuiCol_TabHovered]             = (ImVec4){0.14f, 0.14f, 0.14f, 1.00f};
+  colors[ImGuiCol_TabActive]              = (ImVec4){0.20f, 0.20f, 0.20f, 0.36f};
+  colors[ImGuiCol_TabUnfocused]           = (ImVec4){0.00f, 0.00f, 0.00f, 0.52f};
+  colors[ImGuiCol_TabUnfocusedActive]     = (ImVec4){0.14f, 0.14f, 0.14f, 1.00f};
+  //colors[ImGuiCol_DockingPreview]         = (ImVec4){0.33f, 0.67f, 0.86f, 1.00f};
+  //colors[ImGuiCol_DockingEmptyBg]         = (ImVec4){1.00f, 0.00f, 0.00f, 1.00f};
+  colors[ImGuiCol_PlotLines]              = (ImVec4){1.00f, 0.00f, 0.00f, 1.00f};
+  colors[ImGuiCol_PlotLinesHovered]       = (ImVec4){1.00f, 0.00f, 0.00f, 1.00f};
+  colors[ImGuiCol_PlotHistogram]          = (ImVec4){1.00f, 0.00f, 0.00f, 1.00f};
+  colors[ImGuiCol_PlotHistogramHovered]   = (ImVec4){1.00f, 0.00f, 0.00f, 1.00f};
+  colors[ImGuiCol_TableHeaderBg]          = (ImVec4){0.00f, 0.00f, 0.00f, 0.52f};
+  colors[ImGuiCol_TableBorderStrong]      = (ImVec4){0.00f, 0.00f, 0.00f, 0.52f};
+  colors[ImGuiCol_TableBorderLight]       = (ImVec4){0.28f, 0.28f, 0.28f, 0.29f};
+  colors[ImGuiCol_TableRowBg]             = (ImVec4){0.00f, 0.00f, 0.00f, 0.00f};
+  colors[ImGuiCol_TableRowBgAlt]          = (ImVec4){1.00f, 1.00f, 1.00f, 0.06f};
+  colors[ImGuiCol_TextSelectedBg]         = (ImVec4){0.20f, 0.22f, 0.23f, 1.00f};
+  colors[ImGuiCol_DragDropTarget]         = (ImVec4){0.33f, 0.67f, 0.86f, 1.00f};
+  colors[ImGuiCol_NavHighlight]           = (ImVec4){1.00f, 0.00f, 0.00f, 1.00f};
+  colors[ImGuiCol_NavWindowingHighlight]  = (ImVec4){1.00f, 0.00f, 0.00f, 0.70f};
+  colors[ImGuiCol_NavWindowingDimBg]      = (ImVec4){1.00f, 0.00f, 0.00f, 0.20f};
+  colors[ImGuiCol_ModalWindowDimBg]       = (ImVec4){1.00f, 0.00f, 0.00f, 0.35f};
+
+  ImGuiStyle* style = igGetStyle();
+  //style->WindowPadding                     = (ImVec2){8.00f, 8.00f};
+  //style->FramePadding                      = (ImVec2){5.00f, 2.00f};
+  //style->CellPadding                       = (ImVec2){6.00f, 6.00f};
+  style->ItemSpacing                       = (ImVec2){6.00f, 6.00f};
+  //style->ItemInnerSpacing                  = (ImVec2){6.00f, 6.00f};
+  //style->TouchExtraPadding                 = (ImVec2){0.00f, 0.00f};
+  style->IndentSpacing                     = 25;
+  style->ScrollbarSize                     = 15;
+  style->GrabMinSize                       = 10;
+  style->WindowBorderSize                  = 0;
+  style->ChildBorderSize                   = 0;
+  style->PopupBorderSize                   = 1;
+  style->FrameBorderSize                   = 0;
+  style->TabBorderSize                     = 1;
+  style->WindowRounding                    = 0;
+  style->ChildRounding                     = 4;
+  style->FrameRounding                     = 0;
+  style->PopupRounding                     = 4;
+  style->ScrollbarRounding                 = 9;
+  style->GrabRounding                      = 3;
+  style->LogSliderDeadzone                 = 4;
+  style->TabRounding                       = 4;
+}
 static void init(void) {
 
   #if defined(EMSCRIPTEN)
@@ -1105,7 +1188,7 @@ static void init(void) {
   });
   stm_setup();
   simgui_setup(&(simgui_desc_t){ .dpi_scale= se_dpi_scale()});
-
+  se_imgui_theme();
   // initial clear color
   gui_state.pass_action = (sg_pass_action) {
       .colors[0] = { .action = SG_ACTION_CLEAR, .value = { 0.0f, 0.5f, 1.0f, 1.0 } }
@@ -1134,32 +1217,57 @@ static void frame(void) {
   float menu_height = 0; 
   /*=== UI CODE STARTS HERE ===*/
   igPushStyleVarVec2(ImGuiStyleVar_FramePadding,(ImVec2){5,5});
+  igPushStyleVarVec2(ImGuiStyleVar_WindowPadding,(ImVec2){0,5});
   if (igBeginMainMenuBar())
   {
-    igText("SkyEmu", (ImVec2){0, 0});
-    
-    if(igButton("Reset",(ImVec2){0, 0})){emu_state.run_mode = SB_MODE_RESET;}
-    if(emu_state.run_mode!=SB_MODE_RUN&&emu_state.run_mode!=SB_MODE_REWIND){
-      if(igButton("Play",(ImVec2){0, 0})){emu_state.run_mode=SB_MODE_RUN;emu_state.step_frames = 1;}
-      if(igButton("Step Frame",(ImVec2){0, 0}))emu_state.run_mode=SB_MODE_STEP;
-    }else{
-      igPushStyleVarVec2(ImGuiStyleVar_ItemSpacing,(ImVec2){1,1});
-      if(igButton("<|<|",(ImVec2){0, 0})){emu_state.run_mode=SB_MODE_REWIND;emu_state.step_frames=1;}
-      if(igButton("||",(ImVec2){0, 0})){emu_state.run_mode=SB_MODE_PAUSE;emu_state.step_frames=1;}
-      if(igButton("|>",(ImVec2){0, 0})){emu_state.run_mode=SB_MODE_RUN;emu_state.step_frames=1;}
-      if(igButton("|>|>",(ImVec2){0, 0})){emu_state.run_mode=SB_MODE_RUN;emu_state.step_frames=2;}
-      igPopStyleVar(1);
-      if(igButton("|>|>|>",(ImVec2){0, 0})){emu_state.run_mode=SB_MODE_RUN;emu_state.step_frames=-1;}
-    }
+    if(emu_state.run_mode==SB_MODE_RUN) igText("%.0f FPS",se_fps_counter(0));
+    else igText("SkyEmu", (ImVec2){0, 0});
 
-    igPushItemWidth(100);
+    if(igButton("Reset",(ImVec2){0, 0})){emu_state.run_mode = SB_MODE_RESET;}
+
+    igSetCursorPosX((width/2)-130);
+    igPushItemWidth(-0.01);
     igSliderFloat("",&gui_state.volume,0,1,"Volume: %.02f",ImGuiSliderFlags_AlwaysClamp);
     igPopItemWidth();
-    if(emu_state.run_mode==SB_MODE_RUN) igText("%.0f FPS",se_fps_counter(0));
+
+    int sel_width =35;
+    igPushStyleVarVec2(ImGuiStyleVar_ItemSpacing,(ImVec2){1,1});
+    igSetCursorPosX((width/4)-sel_width*6/2);
+    igPushItemWidth(sel_width);
+    int curr_toggle = 3;
+    if(emu_state.run_mode==SB_MODE_REWIND&&emu_state.step_frames==2)curr_toggle=0;
+    if(emu_state.run_mode==SB_MODE_REWIND&&emu_state.step_frames==1)curr_toggle=1;
+    if(emu_state.run_mode==SB_MODE_PAUSE)curr_toggle=2;
+    if(emu_state.run_mode==SB_MODE_RUN && emu_state.step_frames==1)curr_toggle=3;
+    if(emu_state.run_mode==SB_MODE_RUN && emu_state.step_frames==2)curr_toggle=4;
+    if(emu_state.run_mode==SB_MODE_RUN && emu_state.step_frames==-1)curr_toggle=5;
+    const char* toggle_labels[]={"<|<|<|", "<|<|", "||", "|>", "|>|>","|>|>|>"};
+    int num_toggles = 6;
+    int next_toggle_id = -1; 
+    ImGuiStyle* style = igGetStyle();
+    for(int i=0;i<num_toggles;++i){
+      bool active_button = i==curr_toggle;
+      if(active_button)igPushStyleColorVec4(ImGuiCol_Button, style->Colors[ImGuiCol_ButtonActive]);
+      if(igButton(toggle_labels[i],(ImVec2){sel_width, 0}))next_toggle_id = i;
+      if(active_button)igPopStyleColor(1);
+
+      if(i==num_toggles-1)igPopStyleVar(1);
+    }
+    switch(next_toggle_id){
+      case 0: {emu_state.run_mode=SB_MODE_REWIND;emu_state.step_frames=2;} ;break;
+      case 1: {emu_state.run_mode=SB_MODE_REWIND;emu_state.step_frames=1;} ;break;
+      case 2: {emu_state.run_mode=SB_MODE_PAUSE;emu_state.step_frames=1;} ;break;
+      case 3: {emu_state.run_mode=SB_MODE_RUN;emu_state.step_frames=1;} ;break;
+      case 4: {emu_state.run_mode=SB_MODE_RUN;emu_state.step_frames=2;} ;break;
+      case 5: {emu_state.run_mode=SB_MODE_RUN;emu_state.step_frames=-1;} ;break;
+    }
+    igPopItemWidth();
+    
+    
     menu_height= igGetWindowHeight();
     igEndMainMenuBar();
   }
-  igPopStyleVar(1);
+  igPopStyleVar(2);
 
   igSetNextWindowPos((ImVec2){0,menu_height}, ImGuiCond_Always, (ImVec2){0,0});
   igSetNextWindowSize((ImVec2){width, height-menu_height*se_dpi_scale()}, ImGuiCond_Always);
