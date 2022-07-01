d577edaaac77d742adc5fa17c691bff55ff0b63e
Skyler Saleh <skylersaleh@gmail.com>
2022-07-01 16:41:45 -0700

    【WINDOWS】Enable forced inlining.

--- src/sb_types.h
+++ src/sb_types.h
@@ -17,6 +17,8 @@
 #include <ctype.h>
 #if defined(__GNUC__) || defined(__clang__)
   #define FORCE_INLINE inline __attribute__((always_inline))
+#elif defined(_MSC_VER)
+  #define FORCE_INLINE __forceinline
 #else
   #define FORCE_INLINE inline
 #endif
