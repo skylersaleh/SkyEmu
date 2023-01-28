5b5f7de81be91c03d273fe0155485c00c9f4dd63
Sky <skylersaleh@gmail.com>
2023-01-27 23:47:36 -0800

    【Frontend】Add Armenian font

--- src/res.c
+++ src/res.c
@@ -2,6 +2,7 @@
 #include "forkawesome.h"
 #ifdef UNICODE_GUI
 #include "noto.h"
+#include "noto_armenian.h"
 #endif
 #include "karla.h"
 #include <stdlib.h>
@@ -13,6 +14,7 @@ const uint8_t* se_get_resource(int res_id, uint64_t* size){
     case SE_KARLA: *size = karla_compressed_size; return (uint8_t*)karla_compressed_data;
 #ifdef UNICODE_GUI
     case SE_NOTO: *size = notosans_cjksc_compressed_size; return (uint8_t*)notosans_cjksc_compressed_data;
+    case SE_NOTO_ARMENIAN: *size = noto_armenian_compressed_size; return (uint8_t*)noto_armenian_compressed_data;
 #endif 
   }
   *size = 0; 
--- src/res.h
+++ src/res.h
@@ -6,6 +6,7 @@
 #define SE_NOTO 0
 #define SE_KARLA 1
 #define SE_FORKAWESOME 2
+#define SE_NOTO_ARMENIAN 3
 
 const uint8_t* se_get_resource(int res_id, uint64_t* size);
 
