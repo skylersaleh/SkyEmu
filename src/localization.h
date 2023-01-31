5f1cae752cb3a659ee05187cd01d2ed35a097039
offtkp <parisoplop@gmail.com>
2023-01-31 22:56:38 +0200

    Change localization.h defines to an enum
    This makes SE_MAX_LANG_VALUE automatically take the max value so that
    there's no need to change it when adding a new language

--- src/localization.h
+++ src/localization.h
@@ -1,23 +1,25 @@
 #ifndef LOCALIZATION_H
 #define LOCALIZATION_H
 
-#define SE_LANG_DEFAULT 0
-#define SE_LANG_ENGLISH 1
-#define SE_LANG_ARABIC  2
-#define SE_LANG_ARMENIAN 3 
-#define SE_LANG_BENGALI 4
-#define SE_LANG_CHINESE 5
-#define SE_LANG_FRENCH  6
-#define SE_LANG_GERMAN  7
-#define SE_LANG_GREEK   8
-#define SE_LANG_HINDI   9
-#define SE_LANG_JAPANESE 10
-#define SE_LANG_KOREAN  11
-#define SE_LANG_ITALIAN 12
-#define SE_LANG_PORTUGESE 13
-#define SE_LANG_RUSSIAN 14
-#define SE_LANG_SPANISH 15
-#define SE_MAX_LANG_VALUE 16
+enum {
+    SE_LANG_DEFAULT,
+    SE_LANG_ENGLISH,
+    SE_LANG_ARABIC,
+    SE_LANG_ARMENIAN,
+    SE_LANG_BENGALI,
+    SE_LANG_CHINESE,
+    SE_LANG_FRENCH,
+    SE_LANG_GERMAN,
+    SE_LANG_GREEK,
+    SE_LANG_HINDI,
+    SE_LANG_JAPANESE,
+    SE_LANG_KOREAN,
+    SE_LANG_ITALIAN,
+    SE_LANG_PORTUGESE,
+    SE_LANG_RUSSIAN,
+    SE_LANG_SPANISH,
+    SE_MAX_LANG_VALUE
+};
 
 void se_set_language(int language_enum);//i.e. SE_LANG_ENGLISH
 const char* se_language_string(int language_enum);//returns "" if language is not supported
