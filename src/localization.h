c62fc9be95007154b3377e334fb5a897b07a1599
Sky <skylersaleh@gmail.com>
2023-06-28 21:15:15 -0700

    【NDS】Move up Danish in the language selection list so that it is in alphabetical order.

--- src/localization.h
+++ src/localization.h
@@ -7,6 +7,7 @@
 #define SE_LANG_ARMENIAN 15 
 #define SE_LANG_BENGALI 20
 #define SE_LANG_CHINESE 25
+#define SE_LANG_DANISH 27
 #define SE_LANG_DUTCH 30
 #define SE_LANG_FRENCH  35
 #define SE_LANG_GERMAN  40
@@ -18,7 +19,6 @@
 #define SE_LANG_PORTUGESE 70
 #define SE_LANG_RUSSIAN 75
 #define SE_LANG_SPANISH 80
-#define SE_LANG_DANISH 85
 #define SE_MAX_LANG_VALUE 86
 
 void se_set_language(int language_enum);//i.e. SE_LANG_ENGLISH
