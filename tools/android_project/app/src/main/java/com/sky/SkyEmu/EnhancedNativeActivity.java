58c4cd9b3701234e25b3ccf6a3d5f06a2654da4f
Sky <skylersaleh@gmail.com>
2023-08-04 16:17:55 -0700

    【Android】Autohide navigation bar

--- tools/android_project/app/src/main/java/com/sky/SkyEmu/EnhancedNativeActivity.java
+++ tools/android_project/app/src/main/java/com/sky/SkyEmu/EnhancedNativeActivity.java
@@ -189,6 +189,38 @@ public class EnhancedNativeActivity extends NativeActivity {
                 }
             });
 
+        int currentApiVersion = android.os.Build.VERSION.SDK_INT;
+
+        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        // This work only for android 4.4+
+        if(currentApiVersion >= Build.VERSION_CODES.KITKAT){
+            getWindow().getDecorView().setSystemUiVisibility(flags);
+
+            // Code below is to handle presses of Volume up or Volume down.
+            // Without this, after pressing volume buttons, the navigation bar will
+            // show up and won't hide
+            final View decorView = getWindow().getDecorView();
+            decorView
+                    .setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener()
+                    {
+                        @Override
+                        public void onSystemUiVisibilityChange(int visibility)
+                        {
+                            if((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0)
+                            {
+                                decorView.setSystemUiVisibility(flags);
+                            }
+                        }
+                    });
+        }
+
+
     }
     @Override
     public void onActivityResult(int requestCode, int resultCode,
