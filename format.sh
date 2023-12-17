68a9afa62bb5777bec66f589ad10a8175a15164b
moonie <67006455+Kim-Dewelski@users.noreply.github.com>
2023-12-17 02:14:17 +0100

    added changes to format file

--- format.sh
+++ format.sh
@@ -4,4 +4,4 @@
 # This is a script as to not accidentally format vendored files.
 # Every new file belonging to our code should be included into this one for formatting purposes.
 
-clang-format -i src/main.c src/gb.h src/gba.h src/arm7.h src/gba_bios.h src/ios_support.h src/lcd_shaders.h src/localization.c src/localization.h src/nds.h src/nds_rom_database.h 
+clang-format -i src/main.c src/gb.h src/gba.h src/arm7.h src/ios_support.h src/localization.c src/localization.h src/nds.h  
