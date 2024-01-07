e695a3aed52775db9c6e65a8fd88ab1acda51c00
skylersaleh <skylersaleh@gmail.com>
2024-01-07 00:36:09 +0000

    Deploying to gh-pages from @ skylersaleh/SkyEmu@9a6a8aff8cc66b50ee14f8d4ad685aa36a2017ca 🚀

--- branch/dev/sw.js
+++ branch/dev/sw.js
@@ -1,5 +1,5 @@
 // Define the cache version
-const CACHE_VERSION = '4072d51655b71908649e472388d97ab997369f3c';
+const CACHE_VERSION = '9a6a8aff8cc66b50ee14f8d4ad685aa36a2017ca';
 
 // Define an array of URLs to cache
 const CACHE_URLS = [
