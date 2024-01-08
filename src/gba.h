skylersaleh/SkyEmu

commit 3c7c5d327ddb95f931f812abb7133300d86bb492

【ARM】Remove IF flag from process_interrupts function

src/gba.h
@@ -3807,7 +3807,7 @@ void gba_tick(sb_emu_state_t* emu, gba_t* gba,gba_scratch_t *scratch){
           int_if &= gba_io_read16(gba,GBA_IE);
           uint32_t ime = gba_io_read32(gba,GBA_IME);
           int_if *= SB_BFE(ime,0,1);
-          arm7_process_interrupts(&gba->cpu, int_if);
+          arm7_process_interrupts(&gba->cpu);
         }
       }
       arm7_exec_instruction(&gba->cpu);

src/arm7.h
@@ -129,7 +129,7 @@ static void arm7_exec_instruction(arm7_t* cpu);
 // Write the dissassembled opcode from mem_address into the out_disasm string up to out_size characters
 static void arm7_get_disasm(arm7_t * cpu, uint32_t mem_address, char* out_disasm, size_t out_size);
 // Used to send an interrupt to the emulated CPU. The n'th set bit triggers the n'th interrupt
-static void arm7_process_interrupts(arm7_t* cpu, uint32_t interrupts);
+static void arm7_process_interrupts(arm7_t* cpu);
 ///////////////////////////////////////////
 // Functions for Internal Implementation //
 ///////////////////////////////////////////
@@ -593,12 +593,12 @@ static FORCE_INLINE void arm7_set_thumb_bit(arm7_t* cpu, bool value){
   cpu->registers[CPSR] &= ~(1<<5);
   if(value)cpu->registers[CPSR]|= 1<<5;
 }
-static FORCE_INLINE void arm7_process_interrupts(arm7_t* cpu, uint32_t interrupts){
+static FORCE_INLINE void arm7_process_interrupts(arm7_t* cpu){
   cpu->wait_for_interrupt=false;
   uint32_t cpsr = cpu->registers[CPSR];
   bool I = ARM7_BFE(cpsr,7,1);
   if(I==0&&cpu->phased_op_id==0){
-    if(SB_UNLIKELY(cpu->log_cmp_file||interrupts==0))return; //Log drives interrupts when enabled
+    if(SB_UNLIKELY(cpu->log_cmp_file))return;
     //Interrupts are enabled when I ==0
     bool thumb = arm7_get_thumb_bit(cpu);
     cpu->registers[R14_irq] = cpu->registers[PC]+4;

src/nds.h
@@ -6895,7 +6895,7 @@ void nds_tick(sb_emu_state_t* emu, nds_t* nds, nds_scratch_t* scratch){
         if(int9_if){
           int9_if &= nds9_io_read32(nds,NDS9_IE);
           uint32_t ime = nds9_io_read32(nds,NDS9_IME);
-          if((ime&0x1)&&int9_if) arm7_process_interrupts(&nds->arm9, int9_if);
+          if((ime&0x1)&&int9_if) arm7_process_interrupts(&nds->arm9);
         }
         if(SB_LIKELY(!nds->arm9.wait_for_interrupt)){
           nds->arm9.i_cycles=0;
@@ -6910,7 +6910,7 @@ void nds_tick(sb_emu_state_t* emu, nds_t* nds, nds_scratch_t* scratch){
           uint32_t ie = nds7_io_read32(nds,NDS7_IE);
           uint32_t ime = nds7_io_read32(nds,NDS7_IME);
           int7_if&=ie;
-          if((ime&0x1)&&int7_if) arm7_process_interrupts(&nds->arm7, int7_if);
+          if((ime&0x1)&&int7_if) arm7_process_interrupts(&nds->arm7);
         }
         arm7_exec_instruction(&nds->arm7);
       }
@@ -6919,10 +6919,9 @@ void nds_tick(sb_emu_state_t* emu, nds_t* nds, nds_scratch_t* scratch){
     nds->mem.slow_bus_cycles = 0; 
     
     while(ticks){
-      int ppu_fast_forward = nds->ppu_fast_forward_ticks;
-      if(nds->gpu.cmd_busy_cycles&&nds->gpu.cmd_busy_cycles<=ppu_fast_forward)ppu_fast_forward=nds->gpu.cmd_busy_cycles; 
-      int timer_fast_forward = nds->next_timer_clock-nds->current_clock;
-      int fast_forward_ticks=ppu_fast_forward<timer_fast_forward?ppu_fast_forward:timer_fast_forward; 
+      int fast_forward_ticks = nds->next_timer_clock-nds->current_clock;
+      if(fast_forward_ticks>nds->ppu_fast_forward_ticks)fast_forward_ticks=nds->ppu_fast_forward_ticks;
+      if(nds->gpu.cmd_busy_cycles&&nds->gpu.cmd_busy_cycles<=fast_forward_ticks)fast_forward_ticks=nds->gpu.cmd_busy_cycles; 
       if(SB_LIKELY(fast_forward_ticks)){
         if(SB_UNLIKELY(nds->active_if_pipe_stages)){
           int i=0;

commit 689ebb2bd4ca22abe1602ef6a7ca0fa81b71bd2d

【GBA】Fixed regression that would cause pokemon to not save
Regression originally occured in: 3c7c5d327ddb95f931f812abb7133300d86bb492

src/gba.h
@@ -3807,7 +3807,7 @@ void gba_tick(sb_emu_state_t* emu, gba_t* gba,gba_scratch_t *scratch){
           int_if &= gba_io_read16(gba,GBA_IE);
           uint32_t ime = gba_io_read32(gba,GBA_IME);
           int_if *= SB_BFE(ime,0,1);
-          arm7_process_interrupts(&gba->cpu);
+          if(int_if)arm7_process_interrupts(&gba->cpu);
         }
       }
       arm7_exec_instruction(&gba->cpu);