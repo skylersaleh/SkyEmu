3b3a4b76da34e871472970bf12eecc13d37ae591
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-12-10 20:07:47 +0530

    Update and rename settings.gradle to settings.gradle.kts

---
+++ tools/android_project_kotlin/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":app")
