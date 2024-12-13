7714370b71aa2dc88616ddc98b30e0dfcc21e00a
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-12-13 13:34:24 +0530

    java moment

--- tools/android_project_kotlin/app/src/main/java/com/sky/SkyEmu/EnhancedNativeActivity.java
+++ tools/android_project_kotlin/app/src/main/java/com/sky/SkyEmu/EnhancedNativeActivity.java
@@ -310,7 +310,7 @@ public class EnhancedNativeActivity extends NativeActivity {
         if (extras != null) {
             Game game = extras.getParcelable("game");
             if (game != null) {
-                loadURI(Uri.parse(game.path), true);
+                loadURI(Uri.parse(game.getPath()), true);
             }
         }
     }
