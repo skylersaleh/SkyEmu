9e8e98499ed92d17b4654895481a96759e352cde
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-01-25 22:19:01 +0530

    Update build.gradle.kts

--- tools/android_project_kotlin/app/build.gradle.kts
+++ tools/android_project_kotlin/app/build.gradle.kts
@@ -56,7 +56,7 @@ android {
 
     externalNativeBuild {
         cmake {
-            version = "3.18.1"
+            version = "3.22.1"
             path = file("../../../CMakeLists.txt")
         }
     }
